2ea5666bccb701f846c17bc375f452ad13f96e96
Marco5Sou5a <s.marcosfotos@gmail.com>
2025-12-30 18:33:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import streamlit as st
 from sqlalchemy import create_engine, text
 
-st.set_page_config(page_title="Teste Supabase")
-
-st.title("🔌 Teste de Conexão Supabase")
+st.title("Teste de Conexão Supabase")
 
 engine = create_engine(
     st.secrets["DATABASE_URL"],
     connect_args={"sslmode": "require"}
 )
 
-try:
-    with engine.connect() as conn:
-        conn.execute(text("select 1"))
-    st.success("✅ Conectado com sucesso ao Supabase!")
-except Exception as e:
-    st.error("❌ Falha na conexão com o Supabase")
-    st.exception(e)
+with engine.connect() as conn:
+    conn.execute(text("select 1"))
+
+st.success("✅ Conectado com sucesso ao Supabase!")
